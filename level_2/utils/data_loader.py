6eb71788d5cd79aedf5cb8cfade0e11fc0e04ac3
Lumi1717 <ayhahlam623@gmail.com>
2025-04-01 01:51:16 +0400

    added data loader

--- level_2/utils/data_loader.py
+++ level_2/utils/data_loader.py
@@ -1,6 +1,18 @@
+import glob
 import tensorflow as tf
 
 def create_dataset(lr_dir, hr_dir, batch_size=18, patch_size = 64):
+    """Creates a tf.data.Dataset for ESRGAN training.
+    
+    Args:
+        lr_dir: Path to low-resolution images
+        hr_dir: Path to high-resolution images
+        batch_size: Batch size
+        patch_size: Size of random crops
+        
+    Returns:
+        Configured tf.data.Dataset
+    """
     def preprocess_image(lr_path, hr_path):
         lr = tf.image.decode_image(tf.io.read_file(lr_path), dtype=tf.float32)
         hr = tf.image.decode_image(tf.io.read_file(hr_path), dtype=tf.float32)
@@ -19,3 +31,13 @@ def create_dataset(lr_dir, hr_dir, batch_size=18, patch_size = 64):
     
     lr_files = sorted(glob.glob(f"{lr_dir}"))
     hr_files = sorted(glob.glob(f"{hr_dir}"))
+    
+    dataset = tf.data.Dataset.from_tensor_slices((lr_files, hr_files))
+    dataset = dataset.shuffle(1000).map(preprocess_image, num_parallel_calls= tf.data.AUTOTUNE)
+    dataset = dataset.shuffle(1000)
+    dataset = dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    
+    return dataset
+    
--- level_2/utils/init.py
+++ level_2/utils/init.py
@@ -1,3 +1,4 @@
 from .image_utils import upscale_image, train
+from .data_loader import create_dataset
 
-__all__ = ['train', 'upscale_image']
+__all__ = ['train', 'upscale_image', 'create_dataset']
