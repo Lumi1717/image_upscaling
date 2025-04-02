a7f9046070104e882df063da2d209fe60f1ec5e9
Lumi1717 <ayhahlam623@gmail.com>
2025-04-02 20:05:08 +0400

    added lr generator

--- level_2/scripts/generate_lr_images.py
+++ level_2/scripts/generate_lr_images.py
@@ -1,31 +1,31 @@
 import os
 import cv2
+import yaml
 import numpy as np
 from tqdm import tqdm
 from multiprocessing import Pool
 
-def downscale_image (lr_path, hr_path, scale_factor =4):
+def downscale_image (hr_path,lr_dir, scale_factor =4):
 
     hr_img = cv2.imread(hr_path)
     if hr_img is None:
-        print("DID NOT READ {hr_path} fix yo shit")
+        print(f"DID NOT READ {hr_path} fix yo shit")
         return
     
     #ceate LR imgsss
-
     h, w = hr_img.shape[:2]
     lr_img = cv2.resize(hr_img, (w//scale_factor, h//scale_factor), interpolation= cv2.INTER_CUBIC )
 
     #save imgs
-
     file_name = os.path.basename(hr_path)
-    lr_path = os.path.join(lr_path, file_name)
-    cv2.imread(lr_path, lr_img)
+    lr_path = os.path.join(lr_dir, file_name)
+    cv2.imwrite(lr_path, lr_img)
 
 def process_args(args):
     downscale_image(*args)
 
 def generate_lr_imgs(hr_dir, lr_dir, scale_factor=4, num_workers =4):
+
     os.makedirs(lr_dir, exist_ok=True)
 
     hr_img = [os.path.join(hr_dir, f) for f in os.listdir(hr_dir)
@@ -41,20 +41,23 @@ def generate_lr_imgs(hr_dir, lr_dir, scale_factor=4, num_workers =4):
     
     print(f"Generated {len(hr_img)} LR images in {lr_dir}")
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--hr_dir", required=True, help="Path to HR images")
-    parser.add_argument("--lr_dir", required=True, help="Output path for LR images")
-    parser.add_argument("--scale", type=int, default=4, help="Downscaling factor")
-    parser.add_argument("--workers", type=int, default=4, help="Number of parallel workers")
-    args = parser.parse_args()
-    
-    generate_lr_imgs(args.hr_dir, args.lr_dir, args.scale, args.workers)
+def get_project_root():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.dirname(os.path.dirname(script_dir))
 
-#     cd /Users/ahlamyusuf/Documents/image_upscaling/level_2
-# python scripts/generate_lr_images.py \
-#     --hr_dir data/training/hr/DIV2K_train_HR \
-#     --lr_dir data/lr/training \
-#     --scale 4 \
-#     --workers 8
+if __name__ == '__main__':
+
+    project_root = get_project_root()
+    
+    with open(os.path.join(project_root, "level_2/configs/train_config.yaml"), 'r') as f:
+        config = yaml.safe_load(f)
+    
+    hr_dir = os.path.join(project_root, config['data']['train_hr_dir'])
+    lr_dir = os.path.join(project_root, config['data']['train_lr_dir'])
+    scale = config['data']['scale_factor']
+    workers = config['data']['num_workers']
+
+    print(f"Processing images from: {hr_dir}")
+    print(f"Saving results to: {lr_dir}")
+  
+    generate_lr_imgs(hr_dir, lr_dir, scale, workers)
